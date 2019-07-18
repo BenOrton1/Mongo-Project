8cec3b53923423ff481f1c84b201a9306fa30fc1
Benjamin Orton <b.r.orton@gmail.com>
2019-07-18 10:29:06 +0100

    fixed show() hide() wrong way round

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -2,12 +2,12 @@ $(document).ready(function () {
     $(".allergen-form").hide();
     console.log('this');
     $("#search-Allergens").click(function () {
-        $(".allergen-form").hide("slow");
-        $(".name-form").show("slow");
+        $(".allergen-form").show(1000,"linear");
+        $(".name-form").hide(1000,"linear");
     });
 
     $("#search-name").click(function () {
-        $(".allergen-form").show("slow");
-        $(".name-form").hide("slow");
+        $(".allergen-form").hide(1000,"linear");
+        $(".name-form").show(1000);
     });
 });
